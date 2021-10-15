737e9f7855955541458d56e5c3b057798f040c62
Gabriel Peña <penagabriel22@gmail.com>
2021-10-14 23:56:27 -0300

    prod

--- app.js
+++ app.js
@@ -1,47 +1,17 @@
 require('dotenv').config();
 
 const express = require('express');
-const hbs = require('hbs');
 
 const app = express();
 const port = process.env.PORT;
 
-//Handlebars
-app.set('view engine', 'hbs');
-hbs.registerPartials(__dirname + '/views/partials', function (err) {});
-
-app.get('/', (req, res) => {
-    res.render('home',{
-        nombre: 'Gabriel',
-        titulo: 'Cursito de node'
-    });
-})
-
-app.get('/elements', (req, res) => {
-    res.render('elements',{
-        nombre: 'Gabriel',
-        titulo: 'Elements'
-    });
-})
-
-app.get('/generic', (req, res) => {
-    res.render('generic',{
-        nombre: 'Gabriel',
-        titulo: 'Generic'
-    });
-})
-
 //Servir contenido estatico
-app.use(express.static('views'));
-
-app.get('/hola-mundo', (req, res) => {
-    res.send('HOME!')
-})
+app.use(express.static('public'));
 
 app.get('*', (req, res) => {
-    res.sendFile(__dirname+'/public/404.html');
+    res.sendFile(__dirname+'/public/index.html');
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Example app listening at https://iamgabo.cl`)
 })
